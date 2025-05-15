953758d1867e5bbe67d45e2a61e714b41ed3e8f3
Chitra <chitradvij@gmail.com>
2025-05-15 21:12:11 +0530

    sorted string

--- src/Streams/SortedExample.java
+++ src/Streams/SortedExample.java
@@ -3,6 +3,7 @@ package Streams;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortedExample {
     public static void main(String[] args) {
@@ -17,6 +18,15 @@ public class SortedExample {
         //sort based on length
 
         listOfString.stream().sorted(Comparator.comparing(String::length).reversed()).forEach(System.out::println);
+        String s= "chitradevij";
+        //sort it using stream
+
+       String sortedString= s.chars()
+                .sorted()
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
+
+       System.out.println(sortedString);
 
     }
 }

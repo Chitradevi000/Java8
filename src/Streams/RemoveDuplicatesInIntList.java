28e08e49e2788ab634f99d445a7c0b0b3f1b8f3b
Chitra <chitradvij@gmail.com>
2025-05-17 02:33:08 +0530

    return 4th non repeat char

--- src/Streams/RemoveDuplicatesInIntList.java
+++ src/Streams/RemoveDuplicatesInIntList.java
@@ -9,11 +9,11 @@ public class RemoveDuplicatesInIntList {
     public static void main(String[] args) {
         List<Integer> listOfIntegers = Arrays.asList(112,112,33,4,5,33,7,8,9,10);
         HashSet<Integer> hashSet = new HashSet<>(listOfIntegers);
-        System.out.println("Plain use " +hashSet);
+        System.out.println("Plain use of Hashset " +hashSet);
 
         //using stream
 
-        listOfIntegers.stream().distinct().collect(Collectors.toList()).forEach(i->System.out.println("The unique elemets are   "+i));
+        listOfIntegers.stream().distinct().forEach(i->System.out.println("The unique elemets are   "+i));
 
 
 
--- src/Streams/Return4thCharNonRepeative.java
+++ src/Streams/Return4thCharNonRepeative.java
@@ -1,17 +1,25 @@
 package Streams;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Return4thCharNonRepeative {
     public static void main(String[] args) {
         String str = "I am a Java Developer";
-        List<Character> charList = str.chars().mapToObj(i -> (char) i).collect(Collectors.toList());
+        List<Character> charList = str.chars().mapToObj(i -> (char) i).toList();
 
         char fourthChar = charList.stream()
                 .filter(i -> Collections.frequency(charList, i) == 1)
                 .limit(4).skip(3).findFirst().get();
         System.out.println(fourthChar);
+
+        //another method
+       Character charResult= charList.stream().
+                collect(Collectors.groupingBy(
+                        a->a, LinkedHashMap::new,Collectors.counting()
+
+                )).entrySet()
+                .stream().filter(e->e.getValue()==1).map(e->e.getKey())
+                .limit(4).skip(3).findFirst().get();
+       System.out.println(charResult);
     }}

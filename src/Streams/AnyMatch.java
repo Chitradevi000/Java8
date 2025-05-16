95d76eac8ef75ace36e719496c37ad488b75bfa2
Chitra <chitradvij@gmail.com>
2025-05-16 14:56:04 +0530

    anymatch and filter in streams

--- src/Streams/AnyMatch.java
+++ src/Streams/AnyMatch.java
@@ -2,11 +2,14 @@ package Streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnyMatch {
     public static void main(String[] args) {
         List<Character> listOfChar= Arrays.asList('c','C','v','b');
         boolean result=   listOfChar.stream().anyMatch(c->c=='t');
+        Long countresult = listOfChar.stream().filter(c -> c == 'c').count();
         System.out.println(result);
+        System.out.println(countresult);
     }
 }
